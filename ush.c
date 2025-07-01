3fe8338be5d1a1f123fd14516918664d717e05fc
uchitsa <uchitsa@ya.ru>
2025-07-02 02:41:02 +0300

    init

---
+++ ush.c
@@ -0,0 +1,10 @@
+//
+// Created by uchitsa on 02.07.2025.
+//
+
+#include "ush.h"
+
+int main(int argc, char **argv) {
+    ush_loop();
+    return EXIT_SUCCESS;
+}
---
+++ ush.h
@@ -0,0 +1,8 @@
+//
+// Created by uchitsa on 02.07.2025.
+//
+
+#ifndef USH_USH_H
+#define USH_USH_H
+
+#endif //USH_USH_H
